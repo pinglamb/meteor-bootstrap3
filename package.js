ab52f42d2d3fa6544bece9097d491fc01f3b010f
Gary Or <garyor@eventxtra.com>
2014-08-28 23:21:47 +0800

    Added back jquery dependency and version bump

--- package.js
+++ package.js
@@ -1,10 +1,12 @@
 Package.describe({
   summary: "Front-end framework from Twitter, version 3",
-  version: "3.2.0",
+  version: "3.2.1",
   git: "https://github.com/pinglamb/meteor-bootstrap3.git"
 });
 
 Package.on_use(function (api) {
+  api.use('jquery@1.0.0')
+
   var path = Npm.require('path');
   api.add_files(path.join('css', 'bootstrap.css'), 'client');
   api.add_files(path.join('js', 'bootstrap.js'), 'client');
