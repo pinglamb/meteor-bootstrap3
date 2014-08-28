02f2fe0272b8386d54a030166eb56a2e3af25316
Gary Or <garyor@eventxtra.com>
2014-08-28 23:02:54 +0800

    Initial Setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bootstrap 3 for Meteor
+
+[Bootstrap](http://getbootstrap.com) v3.2.0.
+It works for the latest Meteor 0.9 packaging system.
+
+## Installation
+
+```
+meteor add pinglamb:bootstrap3
+```
+That's it. Enjoy.
+
+## License
+
+This package is licensed under the MIT license.
---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: "Front-end framework from Twitter, version 3",
+  version: "3.2.0",
+  git: "https://github.com/pinglamb/meteor-bootstrap3.git"
+});
+
+Package.on_use(function (api) {
+  api.use('jquery');
+
+  var path = Npm.require('path');
+  api.add_files(path.join('css', 'bootstrap.css'), 'client');
+  api.add_files(path.join('js', 'bootstrap.js'), 'client');
+  api.add_files(path.join('fonts', 'glyphicons-halflings-regular.eot'), 'client');
+  api.add_files(path.join('fonts', 'glyphicons-halflings-regular.svg'), 'client');
+  api.add_files(path.join('fonts', 'glyphicons-halflings-regular.ttf'), 'client');
+  api.add_files(path.join('fonts', 'glyphicons-halflings-regular.woff'), 'client');
+});
